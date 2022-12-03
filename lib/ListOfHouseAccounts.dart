9d724851ca310f8df06d5b604d09b214060a1f6f
Haifa-saud <105865534+Haifa-saud@users.noreply.github.com>
2022-12-03 20:27:38 +0300

    1st push

---
+++ lib/ListOfHouseAccounts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListOfHouseAccounts extends StatefulWidget {
+  const ListOfHouseAccounts({super.key});
+
+  @override
+  State<ListOfHouseAccounts> createState() => _ListOfHouseAccountsState();
+}
+
+class _ListOfHouseAccountsState extends State<ListOfHouseAccounts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
