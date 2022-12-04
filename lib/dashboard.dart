d77e57bd0c0119387b65760678b0098556078c08
Haifa-saud <105865534+Haifa-saud@users.noreply.github.com>
2022-12-04 21:27:46 +0300

    gridView
    figure out why is it erasing everything

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -11,6 +11,14 @@ class dashboard extends StatefulWidget {
 
 class _dashboardState extends State<dashboard> {
   @override
+  List text = [
+    [
+      'اجمالي استهلاك الطاقة',
+      '150 kWh',
+      'تم بلوغ 50% من هدف الشهر',
+    ],
+    ['فاتورة الكهرباء', '500.25 SR', '']
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -59,126 +67,96 @@ class _dashboardState extends State<dashboard> {
       ),
       body: Container(
         transformAlignment: Alignment.topRight,
-        child: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            Container(
-                padding: const EdgeInsets.fromLTRB(6, 0, 0, 0),
-                child: Column(children: [
-                  TextFormField(
-                    // maxLength: 20,
+        child: ListView(padding: const EdgeInsets.all(20), children: [
+          Container(
+              padding: const EdgeInsets.fromLTRB(6, 0, 0, 0),
+              child: Column(children: [
+                TextFormField(
+                  readOnly: true,
+                  textAlign: TextAlign.right,
+                  decoration: const InputDecoration(
+                    hintText: ' لوحة المعلومات',
+                    hintStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black),
+                    contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                    border: InputBorder.none,
+                  ),
+                ),
+                TextFormField(
+                  // maxLength: 20,
+                  readOnly: true,
+                  textAlign: TextAlign.right,
+                  decoration: const InputDecoration(
+                    hintText: ' شهر نوفمبر',
+                    hintStyle: TextStyle(fontSize: 15),
+                    contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ])),
+          Container(
+              padding: const EdgeInsets.fromLTRB(6, 12, 6, 12),
+              child: Material(
+                  elevation: 20,
+                  borderRadius: BorderRadius.circular(30),
+                  child: TextFormField(
                     readOnly: true,
+                    maxLines: 4,
                     textAlign: TextAlign.right,
-                    decoration: const InputDecoration(
-                      hintText: ' لوحة المعلومات',
-                      hintStyle:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                      contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                  TextFormField(
-                    // maxLength: 20,
-                    readOnly: true,
-                    textAlign: TextAlign.right,
-                    decoration: const InputDecoration(
-                      hintText: ' شهر نوفمبر',
-                      hintStyle: TextStyle(fontSize: 15),
-                      contentPadding: EdgeInsets.fromLTRB(20, 0, 20, 10),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ])),
-            Container(
-                padding: const EdgeInsets.fromLTRB(6, 12, 6, 12),
-                child: TextFormField(
-                  // maxLength: 20,
-
-                  readOnly: true,
-                  maxLines: 4,
-                  textAlign: TextAlign.right,
-                  decoration: InputDecoration(
-                    hintText: 'الهدف لإجمالي استهلاك \n :الطاقة',
-                    hintStyle: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w500),
-                    contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        borderSide: const BorderSide(color: Colors.grey)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40.0),
-                        borderSide: BorderSide(color: Colors.grey.shade400)),
-                    errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide:
-                            const BorderSide(color: Colors.red, width: 2.0)),
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide:
-                            const BorderSide(color: Colors.red, width: 2.0)),
-                    prefixIcon: const Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          '300 kWh',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w500),
-                        )),
-                  ),
-                )),
-            Container(
-                padding: const EdgeInsets.fromLTRB(6, 12, 6, 12),
-                child: TextFormField(
-                  // maxLength: 20,
-                  readOnly: true,
-                  textAlign: TextAlign.right,
-                  decoration: InputDecoration(
-                    hintText: 'المزرعة',
-                    contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide: const BorderSide(color: Colors.grey)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide: BorderSide(color: Colors.grey.shade400)),
-                    errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide:
-                            const BorderSide(color: Colors.red, width: 2.0)),
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(100.0),
-                        borderSide:
-                            const BorderSide(color: Colors.red, width: 2.0)),
-                    prefixIcon: const Icon(Icons.arrow_back_ios),
-                  ),
-                )),
-            Container(
-                padding: const EdgeInsets.fromLTRB(6, 12, 6, 12),
-                child: TextFormField(
-                  // maxLength: 20,
-                  readOnly: true,
-                  textAlign: TextAlign.right,
-                  decoration: InputDecoration(
-                      hintText: 'الاستراحة',
-                      contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    decoration: InputDecoration(
+                      hintText: 'الهدف لإجمالي استهلاك \n :الطاقة',
+                      hintStyle: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                      contentPadding: const EdgeInsets.fromLTRB(20, 10, 5, 10),
                       focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100.0),
+                          borderRadius: BorderRadius.circular(30),
                           borderSide: const BorderSide(color: Colors.grey)),
                       enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100.0),
+                          borderRadius: BorderRadius.circular(30.0),
                           borderSide: BorderSide(color: Colors.grey.shade400)),
                       errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100.0),
+                          borderRadius: BorderRadius.circular(30.0),
                           borderSide:
                               const BorderSide(color: Colors.red, width: 2.0)),
                       focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100.0),
+                          borderRadius: BorderRadius.circular(30.0),
                           borderSide:
                               const BorderSide(color: Colors.red, width: 2.0)),
-                      prefixIcon: const Icon(Icons.arrow_back_ios)),
-                )),
-          ],
-        ),
+                      prefixIcon: const Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Text(
+                            '300 kWh',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          )),
+                    ),
+                  ))),
+          // GridView.builder(
+          //   itemCount: 2,
+          //crossAxixSpacing:3,
+          //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          //       crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+          //   itemBuilder: (BuildContext context, int index) {
+          //     final item = text[index];
+
+          //     return buildCard(item);
+          //   },
+          // )
+        ]),
       ),
     );
+  }
+
+  Widget buildCard(List content) {
+    return Container(
+        child: GridTile(
+      header: Text(content[0]),
+      footer: Text(content[1]),
+      child: Center(child: Text(content[2])),
+    ));
   }
 }
